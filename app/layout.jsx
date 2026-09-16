08f5630caca34b569d724a8f814ab53fd1966e8a
Thanh Tai <taidoannguyenthanh20@gmail.com>
2026-09-17 05:31:04 +0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,1 @@
-import "./globals.css";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="vi">
-      <body>
-        <Navbar />
-
-        <main>{children}</main>
-
-        <Footer />
-      </body>
-    </html>
-  );
-}
